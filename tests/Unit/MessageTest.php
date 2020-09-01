3412fba8b48318c7a229bf643c8ad453c1522a63
Carlos Xu <srj520w@gmail.com>
2020-09-01 14:44:12 +0800

    修改model 测试文件

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-//use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
 
 class MessageTest extends TestCase
@@ -12,14 +11,9 @@ class MessageTest extends TestCase
 
     public function testModel()
     {
-//        $message = new Message();
-//        $message->name = '张三';
-//        $message->content = '我是张三';
-//        $message->save();
-        $user = factory(\App\Model\Message::class)->create();
-        $this->assertNotNull($user->name);
-
-
+        $message = factory(\App\Model\Message::class)->create();
+        $this->assertNotNull($message->name);
+        $this->assertNotNull($message->content);
     }
 
 
